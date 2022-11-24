e11cbb1fc50dfab20f07b1a2b2eb6502ea847acf
spxrtxk <spartakvasilev1999@gmail.com>
2022-11-24 16:36:01 +0600

    added fonts

--- tasks/bs_html.js
+++ tasks/bs_html.js
@@ -9,9 +9,9 @@ module.exports = function bs_html() {
 		callbacks: {
 			ready: function (err, bs) {
 				bs.addMiddleware("*", function (req, res) {
-					res.writeHead(302, {
-						location: "404.html"
-					});
+					// res.writeHead(302, {
+					// 	location: "404.html"
+					// });
 					res.end("Redirecting!");
 				});
 			}
