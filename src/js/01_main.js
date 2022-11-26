c898d04f4a992aa9640c2afaba68c26f3a385e96
spxrtxk <spartakvasilev1999@gmail.com>
2022-11-26 21:25:43 +0600

    done all pages w/o adaptive

--- src/js/01_main.js
+++ src/js/01_main.js
@@ -2,6 +2,7 @@ $(document).foundation();
 
 //swipers
 
+//main
 const swiperBanner = new Swiper('.swiper-banner', {
   loop: true,
   pagination: {
@@ -13,6 +14,7 @@ const swiperBanner = new Swiper('.swiper-banner', {
   },
 });
 
+//license
 const swiperLicense = new Swiper('.swiper-license', {
   loop: true,
   slidesPerView: 6,
@@ -26,16 +28,72 @@ const swiperLicense = new Swiper('.swiper-license', {
   },
 });
 
-// services open detail from asper
+//newsdetail thumbs
+var swiperThumbs = new Swiper(".swiper-thumbs", {
+  spaceBetween: 20,
+  slidesPerView: 8,
+  watchSlidesProgress: true,
+});
+//newsdetail
+const swiperNewsDetail = new Swiper('.swiper-newsdetail', {
+  loop: true,
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: swiperThumbs,
+  },
+});
 
+//presentation thumbs
+var swiperThumbs = new Swiper(".swiper-thumbs-presentation", {
+  spaceBetween: 20,
+  slidesPerView: 8,
+  watchSlidesProgress: true,
+});
+//presentation
+const swiperPresentation = new Swiper('.swiper-presentation', {
+  loop: true,
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  thumbs: {
+    swiper: swiperThumbs,
+  },
+});
+
+// services open detail from asper
 const servicesOpenDetailBtn = document.querySelectorAll(".l-services-open");
 
 servicesOpenDetailBtn.forEach((el) => {
-  el.addEventListener("click", openFadeDetail);
+  el.addEventListener("click", openFadeDetail1);
 });
 
-function openFadeDetail(el) {
+function openFadeDetail1(el) {
   let btnTarget = el.currentTarget;
   btnTarget.parentElement.classList.toggle("active");
   btnTarget.parentElement.children[1].classList.toggle("active");
+}
+
+// vacancies open detail from asper
+const vacanciesOpenDetailBtn = document.querySelectorAll(".l-vacancies--more");
+
+vacanciesOpenDetailBtn.forEach((el) => {
+  el.addEventListener("click", openFadeDetail2);
+});
+
+function openFadeDetail2(el) {
+  let btnTarget = el.currentTarget;
+  btnTarget.classList.toggle("active");
+  btnTarget.parentElement.children[2].classList.toggle("active");
 }
